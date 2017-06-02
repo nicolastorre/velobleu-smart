3564a3b2b2b56ac9350311017ac973d5a7b03802
nicolastorre Name <nico.torre.project@gmail.com>
2017-06-02 17:01:49 +0200

    [TASK] Add guzzleHttp client and create "/" route as a proxy of velo bleu web service

--- site/app/routes.php
+++ site/app/routes.php
@@ -1,12 +1,15 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use GuzzleHttp\Client as  GuzzleClient;
 
 // Get all users
 $app->get('/', function () use ($app) {
 
-    $responseData = array("Hello world");
+    $client = new GuzzleClient();
+    $res = $client->request("GET", "https://www.velobleu.org/cartoV2/libProxyCarto.asp");
 
+    $responseData = json_decode($res->getBody()->getContents());
 
     return $app->json($responseData);
-})->bind('api_users');
+})->bind('api_velobleu');
