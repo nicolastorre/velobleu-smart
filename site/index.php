f226e81226101701ce8ab41fa015df65e970baaa
nicolastorre <nico.torre.project@gmail.com>
2017-06-08 14:58:11 +0200

    [TASK] Reorganize structure without web folder to match test server

---
+++ site/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$app = new Silex\Application();
+
+if("velobleu.local" === $_SERVER['SERVER_NAME']) {
+    require __DIR__ . '/app/config/dev.php';
+} else {
+    require __DIR__ . '/app/config/prod.php';
+}
+
+require __DIR__ . '/app/app.php';
+require __DIR__ . '/app/routing.php';
+
+$app->run();
